3898ec2ac7e87c0cc06242ae6b3751f4a24e4900
AitelqadiMo <mohammedaitelqadi32@gmail.com>
2026-07-01 22:45:07 +0200

    feat: add modules/glue-catalog/outputs.tf

---
+++ modules/glue-catalog/outputs.tf
@@ -0,0 +1,14 @@
+output "database_name" {
+  description = "Name of the Glue catalog database."
+  value       = aws_glue_catalog_database.this.name
+}
+
+output "crawler_name" {
+  description = "Name of the Glue crawler."
+  value       = aws_glue_crawler.this.name
+}
+
+output "crawler_role_arn" {
+  description = "ARN of the IAM role assumed by the crawler."
+  value       = aws_iam_role.crawler.arn
+}
